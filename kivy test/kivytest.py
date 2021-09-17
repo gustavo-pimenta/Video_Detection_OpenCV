4395f786474024c19a3325253122a997748289b6
Gustavo Pimenta <gustavopimenta.gp@gmail.com>
2021-09-17 19:42:52 -0300

    Update kivytest.py

--- kivy test/kivytest.py
+++ kivy test/kivytest.py
@@ -26,46 +26,53 @@ class MainApp(MDApp):
 
         layout = MDBoxLayout(orientation='vertical')
         self.image = Image()
-        self.label = MDLabel()
+
+        # layout.add_widget(self.label)
+        # self.label = MDLabel()
+
         layout.add_widget(self.image)
-        layout.add_widget(self.label)
+        
 
         self.save_img_button = MDRaisedButton(
             text="CLICK HERE",
             pos_hint={'center_x': .5, 'center_y': .5},
             size_hint=(None, None))
-        self.save_img_button.bind(on_press = self.show_images)
+        self.save_img_button.bind(on_press = self.update_video_capture)
         layout.add_widget(self.save_img_button)
 
         self.video_capture = cv2.VideoCapture(0) # start video capture
-        Clock.schedule_interval(self.update_video_capture, 1.0/FPS) # interval to call update function
+        Clock.schedule_interval(self.controller, 1.0/FPS) # interval to call update function
         
         return layout
 
-    def update_video_capture(self, *args):
+    def update_video_capture(self):
         
-        conection, frame = self.video_capture.read() # Read the video info
+        conection, self.frame = self.video_capture.read() # Read the video info
+        # self.color_image = self.frame # actual video frame with colors
 
-        self.color_image = frame # actual video frame with colors
-
+    def print_image(self): # show the mais image in the screen
         
-
-        buffer = cv2.flip(frame, 0).tostring()
-        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+        buffer = cv2.flip(self.frame, 0).tostring()
+        texture = Texture.create(size=(self.frame.shape[1], self.frame.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
         self.image.texture = texture
 
-    def grayscale(image):
+    def grayscale(self):
+        self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY) # turn image into grayscale
+        self.frame = cv2.flip(self.frame, 0).tobytes()
+    
+    def duplicate_image(self): # make a duplicate side by side version of an image
 
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # turn image into grayscale
-        return gray_image
+        self.frame = np.hstack((self.frame, self.frame))
+        
+    
+    def controller(self, *args): # controll the functions in use 
 
-    def show_images(self, *args):
-        try:
-            cv2.imshow('frame',self.frame)
-            cv2.imshow('roi image',self.roi_image)
-            cv2.imshow('roi black white',self.roi_blackwhite)
-        except: pass
+        self.update_video_capture()
+        # self.grayscale()
+        self.duplicate_image()
+        self.print_image()
+        
 
     
     

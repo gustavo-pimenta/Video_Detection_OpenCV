e6474fdd2e6fc76264fc5cad9b649a94101cdbe4
Gustavo Pimenta <gustavopimenta.gp@gmail.com>
2021-09-18 18:27:58 -0300

    Update kivytest.py

--- kivy test/kivytest.py
+++ kivy test/kivytest.py
@@ -16,7 +16,7 @@ from kivymd.uix.button import MDRaisedButton
 from kivy.uix.image import Image
 from kivy.graphics.texture import Texture
 from kivy.clock import Clock
-
+from kivy.core.window import Window
 
 FPS = 60.0
 
@@ -25,58 +25,52 @@ class MainApp(MDApp):
     def build(self):
 
         layout = MDBoxLayout(orientation='vertical')
-        self.image = Image()
 
-        # layout.add_widget(self.label)
-        # self.label = MDLabel()
+        self.image = Image() # create Image object
+        layout.add_widget(self.image) # add the Image Widget on screen
+       
+        self.video_capture = VideoCapture()
 
-        layout.add_widget(self.image)
-        
-
-        self.save_img_button = MDRaisedButton(
-            text="CLICK HERE",
-            pos_hint={'center_x': .5, 'center_y': .5},
-            size_hint=(None, None))
-        self.save_img_button.bind(on_press = self.update_video_capture)
-        layout.add_widget(self.save_img_button)
-
-        self.video_capture = cv2.VideoCapture(0) # start video capture
         Clock.schedule_interval(self.controller, 1.0/FPS) # interval to call update function
         
         return layout
-
-    def update_video_capture(self):
         
-        conection, self.frame = self.video_capture.read() # Read the video info
-        # self.color_image = self.frame # actual video frame with colors
+    def controller(self, *args): # controll the functions in use 
 
-    def print_image(self): # show the mais image in the screen
+        self.video_capture.update()
+        self.video_capture.duplicate_image()
         
+        self.image_on_screen()
+    
+    def image_on_screen(self): # show the mais image in the screen
+        
+        self.frame = self.video_capture.get_frame()
+
         buffer = cv2.flip(self.frame, 0).tostring()
         texture = Texture.create(size=(self.frame.shape[1], self.frame.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
         self.image.texture = texture
+
+class VideoCapture():
+
+    def __init__(self): 
+        self.video_capture = cv2.VideoCapture(0) # start video capture
+
+    def update(self):
+        conection, self.frame = self.video_capture.read() # Read the video info
+        # self.color_image = self.frame # actual video frame with colors
+
+    def get_frame(self):
+        return self.frame
 
     def grayscale(self):
         self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY) # turn image into grayscale
         self.frame = cv2.flip(self.frame, 0).tobytes()
     
     def duplicate_image(self): # make a duplicate side by side version of an image
-
         self.frame = np.hstack((self.frame, self.frame))
+    
         
-    
-    def controller(self, *args): # controll the functions in use 
-
-        self.update_video_capture()
-        # self.grayscale()
-        self.duplicate_image()
-        self.print_image()
-        
-
-    
-    
-
 MainApp().run()
 
 
